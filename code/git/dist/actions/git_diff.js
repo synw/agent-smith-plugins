4c8d4603ec2f74e5a5a979b15c445716fae7292f
synw <synwe@yahoo.com>
2025-12-08 17:52:36 +0100

    git: Add verbose and debug logging to git diff action and update version
    - Added verbose and debug logging capabilities to the git diff action to show executed commands and final output
    - Updated package version from 0.0.28 to 0.0.29
    - Fixed minor formatting issue in commit message template

--- code/git/dist/actions/git_diff.js
+++ code/git/dist/actions/git_diff.js
@@ -1,12 +1,21 @@
 import { execute } from "@agent-smith/cli";
 
-async function action(args)
+async function action(args, options)
 {
     const diff = await execute("git", ["diff", ...args]);
+    if (options.verbose || options.debug) {
+        console.log("Executing", "git diff", args.join(" "));
+    }
     let msg = diff;
     const stagedDiff = await execute("git", ["diff", "--staged", ...args]);
+    if (options.verbose || options.debug) {
+        console.log("Executing", "git diff --staged", args.join(" "));
+    }
     if (stagedDiff.length > 0) {
         msg += "\n" + stagedDiff
+    }
+    if (options.debug) {
+        console.log(msg)
     }
     const res = { prompt: msg };
     //console.log("GIT DIFF RES", res)
